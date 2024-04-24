74a365d10d038473496bf8b03d6207ee666147dc
Breno Flaubert de Souza <brenoflaubert@gmail.com>
2024-04-24 14:57:26 -0300

    :sparkles: style: estilizando AuthPage

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,31 +3,34 @@ import { Input } from '@/components/ui/input';
 
 export default function AuthPage() {
   return (
-    <main className="h-screen bg-slate-800">
-      <h1>DashApp</h1>
-      <h3>Acesse sua conta</h3>
-
-      <form action="" className="flex flex-col gap-3">
-        <label htmlFor="">
-          <Input
-            placeholder="email"
-            type="email"
-            className="w-72 bg-slate-300"
-          />
-        </label>
-        <label htmlFor="">
-          <Input
-            placeholder="senha"
-            type="password"
-            className="w-72 bg-slate-300"
-          />
-        </label>
-        <Button className="w-72 h-12 bg-violet-500 hover:bg-violet-700">
-          Entrar
-        </Button>
-      </form>
-
-      <p>Projetado e desenvolvido por Breno Flaubert</p>
+    <main className="h-screen w-full flex bg-slate-800">
+      <section className="w-2/3">
+        <div>Lading content</div>
+      </section>
+      <section className="w-1/3">
+        <h1>DashApp</h1>
+        <h3>Acesse sua conta</h3>
+        <form action="" className="flex flex-col gap-3">
+          <label htmlFor="">
+            <Input
+              placeholder="email"
+              type="email"
+              className="w-72 bg-slate-300"
+            />
+          </label>
+          <label htmlFor="">
+            <Input
+              placeholder="senha"
+              type="password"
+              className="w-72 bg-slate-300"
+            />
+          </label>
+          <Button className="w-72 h-12 bg-violet-500 hover:bg-violet-700">
+            Entrar
+          </Button>
+        </form>
+        <p>Projetado e desenvolvido por Breno Flaubert</p>
+      </section>
     </main>
   );
 }
