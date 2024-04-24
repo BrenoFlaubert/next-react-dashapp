dd1e6d8075a02a80adcf821f2d70e7dfa3e622d8
Breno Flaubert de Souza <brenoflaubert@gmail.com>
2024-04-24 14:47:50 -0300

    :sparkles: feat: adicionando botao do shadcn e estilizando o auth form

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,19 +1,32 @@
+import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 
 export default function AuthPage() {
   return (
-    <main className="h-screen bg-">
+    <main className="h-screen bg-slate-800">
       <h1>DashApp</h1>
       <h3>Acesse sua conta</h3>
-      <form action="">
+
+      <form action="" className="flex flex-col gap-3">
         <label htmlFor="">
-          <Input placeholder="email" type="email" />
+          <Input
+            placeholder="email"
+            type="email"
+            className="w-72 bg-slate-300"
+          />
         </label>
         <label htmlFor="">
-          <Input placeholder="" type="password" />
+          <Input
+            placeholder="senha"
+            type="password"
+            className="w-72 bg-slate-300"
+          />
         </label>
-        <button type="submit">Entrar</button>
+        <Button className="w-72 h-12 bg-violet-500 hover:bg-violet-700">
+          Entrar
+        </Button>
       </form>
+
       <p>Projetado e desenvolvido por Breno Flaubert</p>
     </main>
   );
