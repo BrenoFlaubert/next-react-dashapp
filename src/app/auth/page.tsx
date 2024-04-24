3ae54597755450d9fd68da42427d4dfeba502c15
Breno Flaubert de Souza <brenoflaubert@gmail.com>
2024-04-24 14:27:19 -0300

    :sparkles: feat: criando pagina de autenticacao

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,9 +1,18 @@
-import { AuthForm } from './_components/auth-form';
-
 export default function AuthPage() {
   return (
-    <main>
-      <AuthForm />
+    <main className="h-screen">
+      <h1>DashApp</h1>
+      <h3>Acesse sua conta</h3>
+      <form action="">
+        <label htmlFor="">
+          <input type="email" />
+        </label>
+        <label htmlFor="">
+          <input type="password" />
+        </label>
+        <button type="submit">Entrar</button>
+      </form>
+      <p>Projetado e desenvolvido por Breno Flaubert</p>
     </main>
   );
 }
